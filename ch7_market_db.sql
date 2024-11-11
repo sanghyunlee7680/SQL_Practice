e134db4adb302770570c0171303e48d473884f0b
sanghyun <lsh7680@naver.com>
2024-11-11 18:00:56 +0900

    SQL 7장 +트리거

--- ch7_market_db.sql
+++ ch7_market_db.sql
@@ -208,3 +208,77 @@ end $$
 delimiter ;
 call cursor_proc();
 
+
+-- 트리거
+use ch7_market_db;
+create table if not exists trigger_table 
+( 
+	id int,
+    txt varchar(10)
+)default charset=utf8;
+
+insert into trigger_table values(1, '레드벨벳');
+insert into trigger_table values(2, '잇지');
+insert into trigger_table values(3, '블랙핑크');
+
+delimiter $$
+create trigger myTrigger
+	after delete
+    on trigger_table
+    for each row
+begin
+	set @msg = '가수 그룹이 삭제됨' ; -- 트리거 실행 시 작동되는 코드들
+end $$
+delimiter ;
+
+set @msg = '';
+insert into trigger_table values(4, '마마무');
+select @msg;
+update trigger_table set txt = '블핑' where id = 3;
+select @msg;
+delete from trigger_table where id = 4;
+select @msg;
+
+create table singer
+(
+	select mem_id, mem_name, mem_number, addr
+	from member
+);
+
+create table backup_singer
+(
+	mem_id char(8) not null,
+    mem_name varchar(10) not null,
+    mem_number int not null,
+    addr char(2) not null,
+    modType char(2), -- 변경된 타입. '수정'  또는 '삭제'
+    modDate DATE, -- 변경된 날짜
+    modUser Varchar(30) -- 변경한 사용자
+)default charset=utf8;
+
+delimiter $$
+create trigger singer_updateTrg
+	after update
+    on singer
+    for each row
+begin
+	insert into backup_singer values( old.mem_id, old.mem_name,
+		old.mem_number, old.addr, '수정', curdate(), current_user() );
+end $$
+delimiter ;
+
+delimiter $$
+create trigger singer_deleteTrg
+	after delete
+    on singer
+    for each row
+begin
+	insert into backup_singer values( old.mem_id, old.mem_name,
+		old.mem_number, old.addr, '삭제', curdate(), current_user() );
+end $$
+delimiter ;
+
+update singer SET addr = '영국' where mem_id = 'BLK';
+delete from singer where mem_number >= 7;
+
+select * from backup_singer;
